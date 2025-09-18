eb4c75101374cb6a3c3ab3468d4987d97d018724
mvvalerio <martimvv07@gmail.com>
2025-09-18 11:18:41 +0000

    Done..

---
+++ IntroPython.py
@@ -0,0 +1,58 @@
+# --Instrução condicional
+
+# idade = int(input("Qual a sua idade ?"))
+# 
+# if idade < 18:
+#     print("És maior de idade")
+# elif idade == 18:
+#     print("Acabaste de fazer 18 anos")
+# else:
+#     print("És menor de idade")
+
+# --Laços de repetição
+
+# for i in range(0, 10, 2):
+#     print("Repetição", i)
+# 
+# i = 0
+# while i < 10:
+#     print("Repetição W", i)
+#     i += 2
+
+# --Funções
+
+# def repetir():
+#     i = 0
+#     while i < 10:
+#         print("Repetição", i)
+#         i += 1
+#     return
+# 
+# repetir()
+
+# --Função com parâmetros
+
+# def repetirVezes(vezes):
+#     i = 0
+#     while i < vezes:
+#         print("Repetição", i)
+#         i += 1
+#     return
+# 
+# repetirVezes(5)
+
+# --Funções com retorno
+
+# def somar(a, b):
+#     return a + b
+# 
+# resultado = somar(5, 3)
+# print("Resultado =",resultado)
+
+# Livrarias 
+import math
+
+raiz = math.sqrt(16)
+print("Raiz quadrada de 16 é:", raiz)
+potencia = math.pow(2,3)
+print("2 elevado a 3 é:", potencia)
