04a31f54934c8e53a30623b4c879062d02262739
mvvalerio <martimvv07@gmail.com>
2025-09-22 16:36:51 +0000

    Done

---
+++ Exs.py
@@ -0,0 +1,23 @@
+# List = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+# print(List[11:16])
+
+# -------------
+
+# elemento = ""
+#
+# while elemento != "q":
+#     elemento:str = input("Escreva uma letra: ") 
+#     List.append(elemento)
+# 
+# print(List)
+
+def fibonacci_recursivo(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci_recursivo(n - 1) + fibonacci_recursivo(n - 2)
+
+n = int(input("Quantidade de vezes que quer repetir: "))
+print(f"Fibonacci({n}) = {fibonacci_recursivo(n)}")
