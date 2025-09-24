0879b1d7508de7b37319e459f89307de7dfcd866
mvvalerio <martimvv07@gmail.com>
2025-09-24 12:37:49 +0000

    Done

--- APIrequest.py
+++ APIrequest.py
@@ -1,9 +1,42 @@
 import requests
 
-urlBase = "https://api.agify.io?name="
+def obterIdArea():
+    idArea = input("Escolha uma região 3 - Arq. Açores | 7 - Continente, Arq. Madeira: ")
+    
+    match(idArea):
+        case "3":
+            idArea = "3"
+            url = f"https://api.ipma.pt/open-data/observation/seismic/{idArea}.json"
+            response = requests.get(url)
 
-def getNameInfo(name):
-    url = f"{urlBase}{name}"
-    response = requests.get(url)
-    print(response)
-# To Be Continued
+            if response.status_code == 200:
+                seismic_data = response.json()
+                return seismic_data
+            
+            else:
+                print(f"Failed to retrieve data: {response.status_code}")
+                return None
+            
+        case "7":
+            idArea = "7"  
+            url = f"https://api.ipma.pt/open-data/observation/seismic/{idArea}.json"
+            response = requests.get(url)
+
+            if response.status_code == 200:
+                seismic_data = response.json()
+                return seismic_data
+            
+            else:
+                print(f"Failed to retrieve data: {response.status_code}")
+                return None
+            
+        case _:
+            print("Input incorreto! Tente novamente.")
+            return None
+
+data = obterIdArea()
+if data:
+    print("Dados sísmicos obtidos:", data)
+else:
+    print("Falha ao obter dados.")
+
